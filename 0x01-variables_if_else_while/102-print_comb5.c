76deeff2e55b2e76a4e4b7c9b5fe08da71d2ddba
marangawanjugu <marangawanjugu@gmail,com>
2023-02-20 09:00:52 -0800

    two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,30 @@ int main(void)
 {
 	int i;
 	int j;
+	int a, b, c, d;
 
 	for (i = 0; i <= 99; i++)
 	{
+		a = i / 10;
+		b = i % 10;
+
 		for (j = 1; j <= 99; j++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			if (!(i == 98 && j == 99))
+			c = j / 10;
+			d = j % 10;
+
+			if (a < c || (a == c && b < d))
 			{
-				putchar(',');
+				putchar(a + '0');
+				putchar(b + '0');
 				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+				if (!(i == 98 && j == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
