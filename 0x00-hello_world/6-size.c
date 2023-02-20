f4343cf14f638fd8d81e6a6561ef1c5bf83e9931
marangawanjugu <marangawanjugu@gmail,com>
2023-02-20 10:41:48 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,10 @@
  */
 int main(void)
 {
-	char ch;
-	int i;
-	long int lint;
-	long long int lon;
-	float f;
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(ch));
-	printf("Size of an int: %zu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(lint));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(lon));
-	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
