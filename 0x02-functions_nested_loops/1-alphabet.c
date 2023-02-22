f8d2a272e30fc0412d6d13b8d5f58711f91ceeca
marangawanjugu <marangawanjugu@gmail,com>
2023-02-21 17:49:21 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ * Return - always 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
