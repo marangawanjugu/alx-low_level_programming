87a4ce326c4a87ad805edc2b7632b4172a2675ae
marangawanjugu <marangawanjugu@gmail,com>
2023-02-22 18:32:36 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: void
+ *
+ */
+
+int main(void)
+{
+	int count = 3;
+	long int first = 1, second = 2;
+	long int next = first + second;
+
+	printf("%lu", first);
+	printf("%lu", second);
+
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu \n", next);
+		}
+		else
+		{
+			printf("%lu, ", next);
+		}
+		first = second;
+		second = next;
+
+		next = first + second;
+		count++;
+	}
+	return (0);
+}
+
