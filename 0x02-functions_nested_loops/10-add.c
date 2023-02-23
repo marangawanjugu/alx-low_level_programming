8d0f0ae0a948daddfec836e1c88151f1766a8006
marangawanjugu <marangawanjugu@gmail,com>
2023-02-22 16:38:41 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @i: first number to be computed
+ * @j: second number to be computed
+ * Return: addition value
+ *
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,9 +18,12 @@ void times_table(void)
 		{
 			int z = i * j;
 
-			if (z == 0)
+			if (z > 0)
 			{
-				_putchar('0');
+                                _putchar(',');
+			       	_putchar(' ');
+                                _putchar(z / 10 + '0');
+                                _putchar(z % 10 + '0');
 			}
 			else if (z <= 9)
 			{
@@ -31,10 +34,7 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
+				_putchar('0');
 			}
 		}
 		_putchar('\n');
