cb36aa6b8b789d0d137745eed6bf5d208c5ba758
marangawanjugu <marangawanjugu@gmail,com>
2023-03-10 14:42:25 -0800

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,42 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer
- * @s: char type pointer
- * Return: integer value else 0
- *
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number.
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
  */
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+	int i;
+	int h, p;
 
-	if (s[i] == '-' || s[i] == '+')
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		sign = 1 - 2 * (s[i++] == '-');
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
 	}
-	for (; s[i] != '\0'; ++i)
-		result = result * 10 + s[i] - '0';
-	return (sign * result);
+	if (p < 0)
+		h *= -1;
+
+	return (h);
 }
