93b4ae598c79be5f080e42ee9cdf9c56cb65551f
marangawanjugu <marangawanjugu@gmail,com>
2023-03-16 17:32:39 -0700

    concatenate a string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,11 +10,11 @@
 
 int _strlen(char *s)
 {
-        int i;
+	int i;
 
-        for (i = 0; s[i] != '\0'; i++)
-                ;
-        return (i);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
 }
 
 /**
