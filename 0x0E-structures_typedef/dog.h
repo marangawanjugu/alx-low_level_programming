fa17b438174ddf94c555e6431b87a5bc83c1d3f1
marangawanjugu <marangawanjugu@gmail,com>
2023-03-21 13:02:48 -0700

    define structure of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+#include <stdlib.h>
+
+/**
+ * struct dog - structure of dog
+ * @name: char type pointer
+ * @age: float type
+ * @owner: char type pointer
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
