b4de24a3d814ea8e1dea7b879f27c8ea421eed18
marangawanjugu <marangawanjugu@gmail,com>
2023-03-22 18:24:34 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -9,5 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
 	f(name);
 }
