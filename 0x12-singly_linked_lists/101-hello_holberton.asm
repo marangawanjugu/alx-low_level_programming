604be64556e5b0bdec70ae1a820da6be133e1762
marangawanjugu <marangawanjugu@gmail,com>
2023-03-30 19:49:39 -0700

    64-bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+global    main
+          extern    printf
+
+main:                                       ; This is called by the C library startup code
+          mov       edi, format
+	  xor	    eax, eax
+          call      printf
+	  mov       eax, 0
+          ret
+format:
+          db        "Hello, Holberton", 0
