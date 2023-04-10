2217841f535de48547fa3f692908bc3909bc01b5
marangawanjugu <marangawanjugu@gmail,com>
2023-04-10 03:39:44 -0700

    binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *print_binary - prints the binary representation of a number
@@ -13,7 +14,7 @@ void print_binary(unsigned long int n)
 
 	for (i = 7; i >= 0; i--)
 	{
-		num = (n >> i) & 1;
+		num = (n >> i) & 1 ? 1 : 0;
 		_putchar('0' + num);
 	}
 }
