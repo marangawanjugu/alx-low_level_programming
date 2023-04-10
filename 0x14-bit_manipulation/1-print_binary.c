053708f5b945183d48d41040c69cfc1215968e1e
marangawanjugu <marangawanjugu@gmail,com>
2023-04-10 13:41:04 -0700

    prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,10 +12,12 @@ void print_binary(unsigned long int n)
 	unsigned long int num;
 	int i;
 
-	for (i = 8; i >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
-		num = (n >> i) & 1 ? 1 : 0;
-		if ((n == 0) | (n == 1))
-			_putchar('0' + num);
+		num = (1 << i);
+		if (n & num)
+			_putchar('1');
+		else
+			_putchar('0');
 	}
 }
