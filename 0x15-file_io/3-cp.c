4045e8ef341479591ce1a7236c196fcbfde0daae
marangawanjugu <marangawanjugu@gmail,com>
2023-04-14 19:35:39 -0700

    copy contents of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <sys/types.h>
-
 /**
  *main - copies the content of a file to another file
  *@argc: number of arguments
@@ -8,19 +6,14 @@
  *Return: an int value
  *
  */
-
 int main(int argc, char *argv[])
 {
 	int source_fd, dest_fd, write_file, read_file;
 	char buf[1024];
-	int file_mode;
-
-	file_mode = 0664;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	source_fd = open(argv[1], O_RDONLY);
 	if (source_fd == -1)
@@ -28,19 +21,17 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, file_mode);
+	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (dest_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	while ((read_file = read(source_fd, buf, 1024)) > 0)
 	{
 		write_file = write(dest_fd, buf, read_file);
 		if (write_file == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
 	if (read_file == -1)
@@ -50,13 +41,11 @@ int main(int argc, char *argv[])
 	}
 	if (close(source_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd), exit(100);
 	}
 	if (close(dest_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd), exit(100);
 	}
 	return (0);
 }
