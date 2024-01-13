9824fbec2e0a4621438baef562fdb1207e81da1a
marangawanjugu <marangawanjugu@gmail.com>
2024-01-12 16:20:37 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number.
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int h, p;
+
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		h *= -1;
+
+	return (h);
+}
 
 /**
  *main - multiplies two numbers
@@ -13,8 +55,7 @@
 
 int main(int argc, char *argv[])
 {
-	int num_1 = atoi(argv[1]);
-	int num_2 = atoi(argv[2]);
+	int mul, num_1, num_2;
 	
 	if (argc != 3)
 	{
@@ -23,7 +64,10 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("%d\n", num_1 * num_2);
+		num_1 = _atoi(argv[1]);
+		num_2 = _atoi(argv[2]);
+		mul = num_1 * num_2;
+		printf("%d\n", mul);
 		return (0);
 	}
 }
