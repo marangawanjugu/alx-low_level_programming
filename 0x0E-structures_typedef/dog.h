26d62ff1f219477155ad752017497ddb6a17d21f
marangawanjugu <marangawanjugu@gmail.com>
2024-01-23 12:50:48 -0800

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  *@age: age of the dog
  *@owner: owner of the dog
  *
- *
+ *Description: Holds the dog data and outlines the type of dog's struct
  */
 
 struct dog
@@ -17,11 +17,5 @@ struct dog
 	char *owner;
 };
 
-/*void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);*/
 
 #endif
