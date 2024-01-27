61259f9613766e91f9974385b88737d4e516ebb0
marangawanjugu <marangawanjugu@gmail.com>
2024-01-27 02:59:19 -0800

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -95,10 +95,10 @@ void print_all(const char * const format, ...)
 				specifiers[j].f(list);
 				separator = ", ";
 			}
-			j++;
+			++j;
 		}
 		j = 0;
-		counter++;
+		++counter;
 	}
 	va_end(list);
 	printf("\n");
