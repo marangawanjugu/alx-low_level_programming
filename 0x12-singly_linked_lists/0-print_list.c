05806b3a1d5b951d32856c6c7c5ffd2b131c8206
marangawanjugu <marangawanjugu@gmail.com>
2024-02-01 16:27:34 -0800

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_list - prints all the elements of a list_t list
+ *@h: pointer to the first node in the linked list
+ *
+ * Return: the number of nodes in a linked list_t list
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("%s\n", h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
