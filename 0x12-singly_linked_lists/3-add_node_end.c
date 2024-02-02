6a5aa15805a6e2158ce9c29f9218cffb618351d7
marangawanjugu <marangawanjugu@gmail.com>
2024-02-01 16:49:33 -0800

    linked lists

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "_strlen.c"
+
+/**
+ *add_node_end - add new node at the end of the linked list
+ *@head: points to the first node in the linked list
+ *@str: input string
+ *
+ *Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);/*duplicate the string*/
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;/*set the next field of new_node to NULL*/
+	if (*head == NULL)/*update the head point to point to the new_node*/
+	{
+		*head = new_node;
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != 0)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
+	}
+	return (new_node);
+}
