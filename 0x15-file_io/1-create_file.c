24a7aa62a9acadd356a144d49b60dd252c02b3d7
marangawanjugu <marangawanjugu@gmail.com>
2024-02-12 12:37:30 -0800

    updating task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,13 +36,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
 		write_contents = write(fd, text_content, _strlen(text_content));
-		if (write_contents == -1)
+		if (write_contents == -1 || write_contents != _strlen(text_content))
 		{
 			close(fd);
 			return (-1);
