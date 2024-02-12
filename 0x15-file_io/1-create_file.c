4e31b1c965b05467fb78a5d18b649e1521275977
marangawanjugu <marangawanjugu@gmail.com>
2024-02-12 12:57:40 -0800

    updating task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,22 +6,6 @@
 #include <sys/stat.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: char type pointer
- * Return: int value
- *
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{}
-	return (i);
-}
-
-/**
  * create_file - creates a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
@@ -31,7 +15,7 @@ int _strlen(char *s)
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_contents;
+	int fd, length, write_contents;
 
 	if (filename == NULL)
 	{
@@ -42,8 +26,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		write_contents = write(fd, text_content, _strlen(text_content));
-		if (write_contents == -1 || write_contents != _strlen(text_content))
+		for (length = 0; text_content[length] != '\0'; length++)
+		{} /*finding the length of the string*/
+		write_contents = write(fd, text_content, length);
+		if (write_contents == -1 || write_contents != length)
 		{
 			close(fd);
 			return (-1);
